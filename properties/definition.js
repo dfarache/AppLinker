225b69274368d77fab51a1ac1c2b54484dabee39
bcf <bcf@qlik.com>
2017-06-01 14:40:44 +0200

    Reusing the circular structure of the properties object to avoid repeating its definition

--- properties/definition.js
+++ properties/definition.js
@@ -32,175 +32,45 @@ define(['qlik', 'ng!$q'], function(qlik, $q) {
     }
 
     var appList = getAppList();
-    return {
+    var maxNumberApps = 5;
+    var properties = {
         type: "items",
         component: "accordion",
         items: {
-            linkedApps1: {
-                type: "items",
-                label: "App 1",
-                items: {
-                    app1: {
-                        type: "string",
-                        label: "Name",
-                        ref: "app1",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function (a,b) {
-                            return appList;
-                        }
-                    },
-                    app1Sheet: {
-                        type: "string",
-                        label: "Target sheet ID",
-                        ref: "app1Sheet",
-                        defaultValue: undefined,
-                        component: "dropdown",
-                        options: function(props) {
-                            return (props.app1.length > 0) ? getSheetsList(props.app1)
-                                : [{ label: '<Select an app>', value: '' }];
-                        }
-                    }
-                }
-            },
-            linkedApps2: {
-                type: "items",
-                label: "App 2",
-                items: {
-                    app2: {
-                        type: "string",
-                        label: "Name",
-                        ref: "app2",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(){
-                            return appList;
-                        }
-                    },
-                    app2Sheet: {
-                        type: "string",
-                        label: "Target sheet ID",
-                        ref: "app2Sheet",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(props) {
-                            return (props.app2.length > 0) ? getSheetsList(props.app2)
-                                : [{ label: '<Select an app>', value: '' }];
-                        }
-                    }
-                }
-            },
-            linkedApps3: {
-                type: "items",
-                label: "App 3",
-                items: {
-                    app3: {
-                        type: "string",
-                        label: "Name",
-                        ref: "app3",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(){
-                            return appList;
-                        }
-                    },
-                    app3Sheet: {
-                        type: "string",
-                        label: "Target sheet ID",
-                        ref: "app3Sheet",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(props) {
-                            return (props.app3.length > 0) ? getSheetsList(props.app3)
-                                : [{ label: '<Select an app>', value: '' }];
-                        }
-                    }
-                }
-            },
-            linkedApps4: {
-                type: "items",
-                label: "App 4",
-                items: {
-                    app4: {
-                        type: "string",
-                        label: "Name",
-                        ref: "app4",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(){
-                            return appList;
-                        }
-                    },
-                    app4Sheet: {
-                        type: "string",
-                        label: "Target sheet ID",
-                        ref: "app4Sheet",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(props) {
-                            return (props.app4.length > 0) ? getSheetsList(props.app4)
-                                : [{ label: '<Select an app>', value: '' }];
-                        }
-                    }
-                }
-            },
-            linkedApps5: {
-                type: "items",
-                label: "App 5",
-                items: {
-                    app5: {
-                        type: "string",
-                        label: "Name",
-                        ref: "app5",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(){
-                            return appList;
-                        }
-                    },
-                    app5Sheet: {
-                        type: "string",
-                        label: "Target sheet ID",
-                        ref: "app5Sheet",
-                        defaultValue: "",
-                        component: "dropdown",
-                        options: function(props) {
-                            return (props.app5.length > 0) ? getSheetsList(props.app5)
-                                : [{ label: '<Select an app>', value: '' }];
-                        }
-                    }
-                }
-            },
-            settings: {
-                uses: "settings"
-                // ,
-                // items: {
-                //     additionalProperties: {
-                //         type: "items",
-                //         label: "Linked Apps",
-                //         items: {
-                //             a: {
-                //                 ref: "hideIfCurrentApp",
-                //                 label: "Hide linked app if it's the current app",
-                //                 type: "boolean",
-                //                 show: !0
-                //             },
-                //             b: {
-                //                 ref: "hideIfNoTransferableSelections",
-                //                 label: "Hide linked app if there are no transferable selections",
-                //                 type: "boolean",
-                //                 show: !0
-                //             },
-                //             c: {
-                //                 ref: "showNonTransferableSelections",
-                //                 label: "Show non-transferable selections",
-                //                 type: "boolean",
-                //                 show: !0
-                //             }
-                //         }
-                //     }
-                // }
+            settings: { uses: 'settings' }
+        }
+    }
+
+    for(var i=1; i<=maxNumberApps; i++){
+        properties.items['linkedApps' + i] = {
+            type: 'items',
+            label: 'App ' + i,
+            items: {}
+        }
+
+        properties.items['linkedApps' + i]['items']['app' + i] = {
+            type: 'string',
+            label: 'Name',
+            ref: 'app1',
+            defaultValue: '',
+            component: 'dropdown',
+            options: function (a,b) {
+                return appList;
+            }
+        }
+
+        properties.items['linkedApps' + i]['items']['app' + i + 'Sheet'] = {
+            type: 'string',
+            label: 'Target sheet ID',
+            ref: 'app1Sheet',
+            defaultValue: undefined,
+            component: 'dropdown',
+            options: function(props) {
+                return (props.app1.length > 0) ? getSheetsList(props.app1)
+                    : [{ label: '<Select an app>', value: '' }];
             }
         }
     }
+    
+    return properties;
 });
